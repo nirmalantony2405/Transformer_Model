379fb7646633d1411d9548b9ea43964e541ec420
nirmalantony2405 <nirmalmercyantony.de@gmail.com>
2026-01-14 14:48:07 +0100

    Refactor tokenization test for BPE tokenizers

--- transformer/tokenization/test_tokenizer.py
+++ transformer/tokenization/test_tokenizer.py
@@ -4,7 +4,6 @@ sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "../..")
 from transformer.tokenization.bpe_tokenizer import BPETokenizer
 from transformer.tokenization.hf_bpe_tokenizer import HuggingfaceBPETokenizer
 
-# Corpus for training
 corpus = [
     "Machine learning helps in understanding complex patterns.",
     "Learning machine languages can be complex yet rewarding.",
@@ -13,17 +12,14 @@ corpus = [
     "Understanding natural language is crucial in machine learning."
 ]
 
-# Test sentence
 sentence = "Machine learning is a subset of artificial intelligence."
 
-# Custom BPE Tokenizer
 custom_bpe = BPETokenizer(vocab_size=64)
 custom_bpe.get_base_vocab(corpus)
 custom_bpe.train(corpus)
 custom_tokens = custom_bpe.tokenize(sentence)
 print("Custom BPE Tokenized Sentence:", custom_tokens)
 
-# Huggingface BPE Tokenizer
 hf_bpe = HuggingfaceBPETokenizer(vocab_size=295)
 hf_bpe.train(corpus)
 hf_tokens = hf_bpe.tokenize(sentence)
